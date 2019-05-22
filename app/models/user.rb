b758546d09a1d965bbaee3424af4808703c921ed
sergocap <systemofadown.2013@yandex.ru>
2019-05-22 15:07:59 +0700

    access_token generated

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+  after_create :generate_access_token
+
   devise :database_authenticatable,
          :recoverable, :rememberable
+
+  def generate_access_token
+    self.update_attribute :access_token, SecureRandom.hex
+  end
 end
