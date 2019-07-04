4e56cb5f324227f969697d4331ee6e9e8650a377
sergocap <systemofadown.2013@yandex.ru>
2019-07-04 20:51:38 +0700

    normal interval for update

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,6 @@
 set :environment, ENV['RAKE_ENV'] || ENV['RAILS_ENV'] || 'development'
 set :output, File.join(Whenever.path, 'log', 'whenever.log')
 
-every 1.minute do
+every 30.minute do
   runner 'Activity.import_from_hubstaff'
 end
